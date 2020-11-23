0ad603501bbd3c032d8cdc932fd58f5cb179e364
Eric Kolve <erick@allenai.org>
2020-11-23 15:42:50 -0800

    restoring example in example_agent.py

--- example_agent.py
+++ example_agent.py
@@ -7,11 +7,6 @@ from pprint import pprint
 
 if __name__ == '__main__':
     startx()
-    c = ai2thor.controller.Controller(renderObjectImage=True, renderClassImage=True)
-    c.reset('FloorPlan28')
-    for i in range(10000):
-        c.step('RotateRight')
-        total = len(c.last_event.metadata['colorBounds'])
-        print("%s length %s"  % (os.getpid(), total))
-        time.sleep(2.0)
-
+    controller = ai2thor.controller.Controller(scene='FloorPlan28')
+    event = controller.step(action='RotateRight')
+    pprint(event.metadata['agent'])
